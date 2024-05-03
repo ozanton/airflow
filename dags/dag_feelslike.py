1beff9354908bac4010fb6f5899342ed7d400706
ozanton <anton.borisenko@gmail.com>
2024-05-03 16:43:25 +0300

    rates up

--- dags/dag_feelslike.py
+++
@@ -1,9 +0,0 @@
-from datetime import datetime
-from airflow.models import DAG
-from airflow.operators.bash import BashOperator
-from airflow.hooks.base_hook import BaseHook
-
-
-
-
-connection = BaseHook.get_connection("weather_postgreSQL_con")
