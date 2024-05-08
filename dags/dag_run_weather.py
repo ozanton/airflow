43c8bf99438cd5996fc68ad0770e1647e6e8da5e
ozanton <anton.borisenko@gmail.com>
2024-05-08 15:38:14 +0300

    up task

--- dags/dag_run_weather.py
+++ dags/dag_run_weather.py
@@ -1,7 +1,10 @@
 from datetime import datetime, timedelta
 from airflow.models import DAG
 from airflow.operators.bash import BashOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.hooks.base_hook import BaseHook
 
+connection = BaseHook.get_connection("weather_postgreSQL_con")
 
 default_args = {
     "owner": "etl_user",
@@ -20,9 +23,41 @@ task1 = BashOperator(
     bash_command='python3 /airflow/scripts/dag_run_weather/task_run_weatherapi.py',
     dag=dag)
 
-# task2 = BashOperator(
-#     task_id='task_run_windyapi',
-#     bash_command='python3 /airflow/scripts/dag_run_weather/task_run_windy.py',
-#     dag=dag)
+temp_table = "weather_temp"
+hum_table = "weather_hum"
+wind_table = "weather_wind"
+wind_dir_table = "weather_winddir"
+cloud_table = "weather_cloud"
 
-task1
+# SQL-запрос
+sql = """
+INSERT INTO {{ dag.dag_id }}."{temp_table}" (id_city, time, temp_c)
+SELECT id_city, time, temp_c FROM weatherapi_current;
+
+INSERT INTO {{ dag.dag_id }}."{hum_table}" (id_city, time, humidity)
+SELECT id_city, time, humidity FROM weatherapi_current;
+
+INSERT INTO {{ dag.dag_id }}."{wind_table}" (id_city, time, wind_kph)
+SELECT id_city, time, wind_kph FROM weatherapi_current;
+
+INSERT INTO {{ dag.dag_id }}."{wind_dir_table}" (id_city, time, wind_direction)
+SELECT id_city, time, wind_direction FROM weatherapi_current;
+
+INSERT INTO {{ dag.dag_id }}."{cloud_table}" (id_city, time, cloud)
+SELECT id_city, time, cloud FROM weatherapi_current;
+"""
+
+task2 = PostgresOperator(
+    task_id='task_transfer_data',
+    postgres_conn_id=connection,
+    sql=sql,
+    dag=dag)
+
+task3 = PostgresOperator(
+    task_id='clear',
+    postgres_conn_id=connection,
+    sql="""DELETE FROM weatherapi_current""",
+    trigger_rule="one_success",
+    dag=dag)
+
+task1 >> task2 >> task3
